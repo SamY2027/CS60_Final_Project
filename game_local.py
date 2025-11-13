ba281380985d7d554c005e994dc532d1bd32320d
Sam Young <samuel.r.young.27@dartmouth.edu>
2025-11-13 13:40:27 -0500

    Separated players' inputs to separate objects. game_logic is its own module

--- game_local.py
+++ game_local.py
@@ -1,244 +1,26 @@
-# Game that works very well for saving state and stepping forward, super deterministic, no floats!
+# Setup for tesing game_logic module functionality separate from the network
+# P1 used asd controls, p2 used jkl (left, attack, right)
+# Also good demonstration of how to setup, get controller input from, and close down pygame
+# as well as how to use game_logic functions and classes
 
 import pygame
-import copy
-from math import sin, cos, radians
-
-# Captures the state of game at any given frame
-class GameState:
-    def __init__(self, p1_x=0, p1_hp=100, p1_atk_frame=0, p2_x=0, p2_hp=100, p2_atk_frame=0, game_state_list=None):
-        if game_state_list == None:
-            self.p1_x = p1_x
-            self.p1_hp = p1_hp
-            self.p1_atk_frame = p1_atk_frame
-            self.p2_x = p2_x
-            self.p2_hp = p2_hp
-            self.p2_atk_frame = p2_atk_frame
-        else:
-            self.load_from_list(game_state_list)
-
-    # Readable string summary of the current game state
-    def __str__(self):
-        summary = ("p1_x: " + str(self.p1_x) + "\n"
-                 + "p1_hp: " + str(self.p1_hp) + "\n"
-                 + "p1_atk_frame: " + str(self.p1_atk_frame) + "\n"
-                 + "p2_x: " + str(self.p2_x) + "\n"
-                 + "p2_hp: " + str(self.p2_hp) + "\n"
-                 + "p2_atk_frame: " + str(self.p2_atk_frame))
-        
-        return summary
-    
-    # Store each game state variable in a list
-    def make_list(self) -> list:
-        return [self.p1_x, self.p1_hp, self.p1_atk_frame, self.p2_x, self.p2_hp, self.p2_atk_frame]
-    
-    # Load the game state from a list in the format ouput by make_list
-    def load_from_list(self, game_state_list):
-        self.p1_x = game_state_list[0]
-        self.p1_hp = game_state_list[1]
-        self.p1_atk_frame = game_state_list[2]
-        self.p2_x = game_state_list[3]
-        self.p2_hp = game_state_list[4]
-        self.p2_atk_frame = game_state_list[5]
-    
-    
-
-# Class captures state of controller at any given frame
-class ControlState:
-    def __init__(self, p1_mv_l=False, p1_mv_r=False, p1_atk=False, p2_mv_l=False, p2_mv_r=False, p2_atk=False, control_state_list=None):
-        if control_state_list == None:
-            self.p1_mv_l = p1_mv_l
-            self.p1_mv_r = p1_mv_r
-            self.p1_atk = p1_atk
-            self.p2_mv_l = p2_mv_l
-            self.p2_mv_r = p2_mv_r
-            self.p2_atk = p2_atk
-        else:
-            self.load_from_list(control_state_list)
-
-    # Readable string summary of stored control state
-    def __str__(self):
-        summary = ("p1_mv_l: " + str(self.p1_mv_l) + "\n"
-                 + "p1_mv_r: " + str(self.p1_mv_r) + "\n"
-                 + "p1_atk: " + str(self.p1_atk) + "\n"
-                 + "p2_mv_l: " + str(self.p2_mv_l) + "\n"
-                 + "p2_mv_r: " + str(self.p2_mv_r) + "\n"
-                 + "p2_atk: " + str(self.p2_atk))
-        
-        return summary
-    
-    # Store each control input state in a list
-    def make_list(self) -> list:
-        return [self.p1_mv_l, self.p1_mv_r, self.p1_atk, self.p2_mv_l, self.p2_mv_r, self.p2_atk]
-
-    # Load a controller state from a list in the format output by make_list
-    def load_from_list(self, control_state_list):
-        self.p1_mv_l = control_state_list[0]
-        self.p1_mv_r = control_state_list[1]
-        self.p1_atk = control_state_list[2]
-        self.p2_mv_l = control_state_list[3]
-        self.p2_mv_r = control_state_list[4]
-        self.p2_atk = control_state_list[5]
-        self.control_state_list = None
-        
-
-WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 400
-
-PLAYER_SIZE = 50
-
-PLAYER_SPEED = 10
-
-ATTACK_FRAMES = 60
-ATTACK_RANGE = 50
-SWORD_THICKNESS = 5
-ATTACK_DAMAGE = 5
-
-BACKGROUND_COLOR = pygame.Color("white")
-P1_COLOR = pygame.Color("blue")
-P2_COLOR = pygame.Color("green")
-SWORD_COLOR = pygame.Color("black")
-
-
-def render_frame(game_state: GameState, window):
-    # Clear Background
-    window.fill(pygame.Color("white"))
-
-    # Draw Player 1 (location is center of square)
-    pygame.draw.rect(window, P1_COLOR, [game_state.p1_x - (PLAYER_SIZE / 2), WINDOW_HEIGHT - PLAYER_SIZE, PLAYER_SIZE, PLAYER_SIZE], 0)
-
-    # Draw player 1 sword (if an attack is active)
-    # Sword starts out pointing 45 degrees backwards out of player and moves to straigt forward over length of attack
-    if game_state.p1_atk_frame != 0:
-        
-        p1_center = [game_state.p1_x, WINDOW_HEIGHT - (PLAYER_SIZE / 2)]
-
-        angle = (135 / ATTACK_FRAMES) * (ATTACK_FRAMES - game_state.p1_atk_frame)
-
-        sword_tip_offset = [cos(radians(angle)) * ATTACK_RANGE, sin(radians(angle)) * ATTACK_RANGE] # Think in regular coordinates, we fix later for screen coords
-
-        pygame.draw.line(window, SWORD_COLOR, p1_center, [p1_center[0] + sword_tip_offset[0], p1_center[1] - sword_tip_offset[1]], SWORD_THICKNESS)
-
-    # Draw Player 2 (location is center of square)
-    pygame.draw.rect(window, P2_COLOR, [game_state.p2_x - (PLAYER_SIZE / 2), WINDOW_HEIGHT - PLAYER_SIZE, PLAYER_SIZE, PLAYER_SIZE], 0)
-
-    # Draw player 2 sword (if an attack is active)
-    # Sword starts out pointing 45 degrees backwards out of player and moves to straigt forward over length of attack
-    if game_state.p2_atk_frame != 0:
-        
-        p2_center = [game_state.p2_x, WINDOW_HEIGHT - (PLAYER_SIZE / 2)]
-
-        angle = 45 + ((135 / ATTACK_FRAMES) * game_state.p2_atk_frame)
-
-        sword_tip_offset = [cos(radians(angle)) * ATTACK_RANGE, sin(radians(angle)) * ATTACK_RANGE] # Think in regular coordinates, we fix later for screen coords
-
-        pygame.draw.line(window, SWORD_COLOR, p2_center, [p2_center[0] + sword_tip_offset[0], p2_center[1] - sword_tip_offset[1]], SWORD_THICKNESS)
-
-    # Display text of all game state information (usefull for debugging)
-    default_font = pygame.font.Font("freesansbold.ttf", 20) # Load default font shipped with pygame
-
-    # Player 1 stats in formatted string
-    p1_stats = f"Player 1 HP: {game_state.p1_hp}\nPlayer 1 X: {game_state.p1_x}\nP1 Attack Frame: {game_state.p1_atk_frame}"
-
-    # Player 2 stats in formatted string
-    p2_stats = f"Player 2 HP: {game_state.p2_hp}\nPlayer 2 X: {game_state.p2_x}\nP2 Attack Frame: {game_state.p2_atk_frame}"
-
-    p1_text = default_font.render(p1_stats, True, (0, 0, 0))
-    p2_text = default_font.render(p2_stats, True, (0, 0, 0))
-
-    p1_textRect = p1_text.get_rect()
-    p1_textRect.center = (100, 50)
-
-    p2_textRect = p2_text.get_rect()
-    p2_textRect.center = (WINDOW_WIDTH - 100, 50)
-
-    window.blit(p1_text, p1_textRect)
-    window.blit(p2_text, p2_textRect)
-
-    pygame.display.update()
-
-# Takes a game state and input state and returns a NEW game state which is next frame based on those inputs
-def update_state(current_game_state: GameState, control_state: ControlState) -> GameState:
-    new_game_state = copy.deepcopy(current_game_state) # Copy current game state but with independent values
-
-    # Move Player 1
-    if control_state.p1_mv_l and not control_state.p1_mv_r: # Will not move if both move controls pressed!
-        new_game_state.p1_x -= PLAYER_SPEED
-        # Check left wall collision
-        if new_game_state.p1_x < PLAYER_SIZE // 2:
-            new_game_state.p1_x = PLAYER_SIZE // 2
-
-    elif control_state.p1_mv_r and not control_state.p1_mv_l:
-        new_game_state.p1_x += PLAYER_SPEED
-        # Check right wall collision
-        if new_game_state.p1_x > WINDOW_WIDTH - (PLAYER_SIZE // 2):
-            new_game_state.p1_x = WINDOW_WIDTH - (PLAYER_SIZE // 2)
-
-    # Process Player 1's attack
-    if current_game_state.p1_atk_frame == 0: # p1 has not started an attack
-        if control_state.p1_atk: # Start an attack if the control is pressed
-            new_game_state.p1_atk_frame += 1
-
-    elif current_game_state.p1_atk_frame < ATTACK_FRAMES: # p1's attack is in progress
-        new_game_state.p1_atk_frame += 1
-
-    elif current_game_state.p1_atk_frame == ATTACK_FRAMES: # p1 has just completed an attack
-        # Process potential damage to p2
-        # Player is damaged if standing within attack range + half of player size, but ahead of p1's center
-        player_distance = new_game_state.p2_x - new_game_state.p1_x
-        if player_distance <= ATTACK_RANGE + (PLAYER_SIZE // 2) and player_distance >= 0:
-            new_game_state.p2_hp -= ATTACK_DAMAGE
-        new_game_state.p1_atk_frame = 0 # reset p1's attack
-
-    # Move Player 2
-    if control_state.p2_mv_l and not control_state.p2_mv_r: # Will not move if both move controls pressed!
-        new_game_state.p2_x -= PLAYER_SPEED
-        # Check left wall collision
-        if new_game_state.p2_x < PLAYER_SIZE // 2:
-            new_game_state.p2_x = PLAYER_SIZE // 2
-
-    elif control_state.p2_mv_r and not control_state.p2_mv_l:
-        new_game_state.p2_x += PLAYER_SPEED
-        # Check right wall collision
-        if new_game_state.p2_x > WINDOW_WIDTH - (PLAYER_SIZE // 2):
-            new_game_state.p2_x = WINDOW_WIDTH - (PLAYER_SIZE // 2)
-
-    # Process Player 2's attack
-    if current_game_state.p2_atk_frame == 0: # p2 has not started an attack
-        if control_state.p2_atk: # Start an attack if the control is pressed
-            new_game_state.p2_atk_frame += 1
-
-    elif current_game_state.p2_atk_frame < ATTACK_FRAMES: # p2's attack is in progress
-        new_game_state.p2_atk_frame += 1
-
-    elif current_game_state.p2_atk_frame == ATTACK_FRAMES: # p2 has just completed an attack
-        # Process potential damage to p1
-        # Player is damaged if standing within attack range + half of player size, but ahead of p2's center
-        player_distance = new_game_state.p2_x - new_game_state.p1_x
-        if player_distance <= ATTACK_RANGE + (PLAYER_SIZE // 2) and player_distance >= 0:
-            new_game_state.p1_hp -= ATTACK_DAMAGE
-        
-        new_game_state.p2_atk_frame = 0 # reset p2's attack
-
-    return new_game_state
-        
+import game_logic
 
 # Testing
 if __name__ == "__main__":
     
     pygame.init()
 
-    window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    window = pygame.display.set_mode((game_logic.WINDOW_WIDTH, game_logic.WINDOW_HEIGHT))
 
     pygame.display.set_caption("Fighting Squares")
 
     clock = pygame.time.Clock()
 
-    default_font = pygame.font.Font("freesansbold.ttf", 20) # This font ships with pygame
+    game_state = game_logic.GameState(p1_x=100, p2_x=700)
 
-    game_state = GameState(p1_x=100, p2_x=700)
-
-    control_state = ControlState() # Game starts with no controls pressed
+    p1_control_state = game_logic.ControlState() # Game starts with no controls pressed
+    p2_control_state = game_logic.ControlState() # Game starts with no controls pressed
 
     # Game Loop
     running = True
@@ -249,36 +31,36 @@ if __name__ == "__main__":
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_a: # a moves p1 left
-                    control_state.p1_mv_l = True
+                    p1_control_state.mv_l = True
                 elif event.key == pygame.K_d: # d moves p1 right
-                    control_state.p1_mv_r = True
+                    p1_control_state.mv_r = True
                 elif event.key == pygame.K_s: # s is p1's attack
-                    control_state.p1_atk = True
+                    p1_control_state.atk = True
                 elif event.key == pygame.K_j: # j moves p2 left
-                    control_state.p2_mv_l = True
+                    p2_control_state.mv_l = True
                 elif event.key == pygame.K_l: # l moves p2 right
-                    control_state.p2_mv_r = True
+                    p2_control_state.mv_r = True
                 elif event.key == pygame.K_k:
-                    control_state.p2_atk = True # k is p2's attack
+                    p2_control_state.atk = True # k is p2's attack
 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_a: # a moves p1 left
-                    control_state.p1_mv_l = False
+                    p1_control_state.mv_l = False
                 elif event.key == pygame.K_d: # d moves p1 right
-                    control_state.p1_mv_r = False
+                    p1_control_state.mv_r = False
                 elif event.key == pygame.K_s: # s is p1's attack
-                    control_state.p1_atk = False
+                    p1_control_state.atk = False
                 elif event.key == pygame.K_j: # j moves p2 left
-                    control_state.p2_mv_l = False
+                    p2_control_state.mv_l = False
                 elif event.key == pygame.K_l: # l moves p2 right
-                    control_state.p2_mv_r = False
+                    p2_control_state.mv_r = False
                 elif event.key == pygame.K_k:
-                    control_state.p2_atk = False # k is p2's attack
+                    p2_control_state.atk = False # k is p2's attack
 
-        render_frame(game_state, window)
-        game_state = update_state(game_state, control_state)
+        game_logic.render_frame(game_state, window)
+        game_state = game_logic.update_state(game_state, p1_control_state, p2_control_state)
 
-        clock.tick(60) # Lock the game at 30fps, all timings are based on framerate, so this controls run speed
+        clock.tick(30) # Lock the game at 30fps, all timings are based on framerate, so this controls run speed
     
     # Quit Pygame
     pygame.quit()
---
+++ game_logic.py
@@ -0,0 +1,223 @@
+# Module to implement the core functionality of a two-player fighting game.
+# Classes to conveniently handle storing variables for state of game and players' 
+# control inputs. 
+# Functions to update game state based on controls and to render the game.
+# Game rendering is independent of the simulation, ideal for netcode experiments.
+
+import pygame
+import copy
+from math import sin, cos, radians
+
+# Game Window Size
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 400
+
+PLAYER_SIZE = 50
+
+PLAYER_SPEED = 10
+
+# Player Attack Constants
+ATTACK_FRAMES = 60
+ATTACK_RANGE = 50
+ATTACK_DAMAGE = 5
+
+# Visual Constants
+SWORD_THICKNESS = 5
+BACKGROUND_COLOR = pygame.Color("white")
+P1_COLOR = pygame.Color("blue")
+P2_COLOR = pygame.Color("green")
+SWORD_COLOR = pygame.Color("black")
+
+# Captures the state of game at any given frame
+# Maintains record of both players' locations, health points and current frame in attack (0 for none ongoing)
+
+class GameState:
+    def __init__(self, p1_x=100, p1_hp=100, p1_atk_frame=0, p2_x=700, p2_hp=100, p2_atk_frame=0, game_state_list=None):
+        if game_state_list == None:
+            self.p1_x = p1_x
+            self.p1_hp = p1_hp
+            self.p1_atk_frame = p1_atk_frame
+            self.p2_x = p2_x
+            self.p2_hp = p2_hp
+            self.p2_atk_frame = p2_atk_frame
+        else:
+            self.load_from_list(game_state_list)
+
+    # Readable string summary of the current game state
+    def __str__(self):
+        summary = ("p1_x: " + str(self.p1_x) + "\n"
+                 + "p1_hp: " + str(self.p1_hp) + "\n"
+                 + "p1_atk_frame: " + str(self.p1_atk_frame) + "\n"
+                 + "p2_x: " + str(self.p2_x) + "\n"
+                 + "p2_hp: " + str(self.p2_hp) + "\n"
+                 + "p2_atk_frame: " + str(self.p2_atk_frame))
+        
+        return summary
+    
+    # Store each game state variable in a list
+    def make_list(self) -> list:
+        return [self.p1_x, self.p1_hp, self.p1_atk_frame, self.p2_x, self.p2_hp, self.p2_atk_frame]
+    
+    # Load the game state from a list in the format ouput by make_list
+    def load_from_list(self, game_state_list):
+        self.p1_x = game_state_list[0]
+        self.p1_hp = game_state_list[1]
+        self.p1_atk_frame = game_state_list[2]
+        self.p2_x = game_state_list[3]
+        self.p2_hp = game_state_list[4]
+        self.p2_atk_frame = game_state_list[5]
+    
+    
+
+# Class captures state of one player's controls on a given frame
+class ControlState:
+    def __init__(self, mv_l=False, mv_r=False, atk=False, control_state_list=None):
+        if control_state_list == None:
+            self.mv_l = mv_l
+            self.mv_r = mv_r
+            self.atk = atk
+
+        else:
+            self.load_from_list(control_state_list)
+
+    # Readable string summary of stored control state
+    def __str__(self):
+        summary = ("mv_l: " + str(self.mv_l) + "\n"
+                 + "mv_r: " + str(self.mv_r) + "\n"
+                 + "atk: " + str(self.atk))
+        
+        return summary
+    
+    # Store each control input state in a list
+    def make_list(self) -> list:
+        return [self.mv_l, self.mv_r, self.atk]
+
+    # Load a controller state from a list in the format output by make_list
+    def load_from_list(self, control_state_list):
+        self.p1_mv_l = control_state_list[0]
+        self.p1_mv_r = control_state_list[1]
+        self.p1_atk = control_state_list[2]
+        self.control_state_list = None
+
+
+def render_frame(game_state: GameState, window):
+    # Clear Background
+    window.fill(pygame.Color("white"))
+
+    # Draw Player 1 (location is center of square)
+    pygame.draw.rect(window, P1_COLOR, [game_state.p1_x - (PLAYER_SIZE / 2), WINDOW_HEIGHT - PLAYER_SIZE, PLAYER_SIZE, PLAYER_SIZE], 0)
+
+    # Draw player 1 sword (if an attack is active)
+    # Sword starts out pointing 45 degrees backwards out of player and moves to straigt forward over length of attack
+    if game_state.p1_atk_frame != 0:
+        
+        p1_center = [game_state.p1_x, WINDOW_HEIGHT - (PLAYER_SIZE / 2)]
+
+        angle = (135 / ATTACK_FRAMES) * (ATTACK_FRAMES - game_state.p1_atk_frame)
+
+        sword_tip_offset = [cos(radians(angle)) * ATTACK_RANGE, sin(radians(angle)) * ATTACK_RANGE] # Think in regular coordinates, we fix later for screen coords
+
+        pygame.draw.line(window, SWORD_COLOR, p1_center, [p1_center[0] + sword_tip_offset[0], p1_center[1] - sword_tip_offset[1]], SWORD_THICKNESS)
+
+    # Draw Player 2 (location is center of square)
+    pygame.draw.rect(window, P2_COLOR, [game_state.p2_x - (PLAYER_SIZE / 2), WINDOW_HEIGHT - PLAYER_SIZE, PLAYER_SIZE, PLAYER_SIZE], 0)
+
+    # Draw player 2 sword (if an attack is active)
+    # Sword starts out pointing 45 degrees backwards out of player and moves to straigt forward over length of attack
+    if game_state.p2_atk_frame != 0:
+        
+        p2_center = [game_state.p2_x, WINDOW_HEIGHT - (PLAYER_SIZE / 2)]
+
+        angle = 45 + ((135 / ATTACK_FRAMES) * game_state.p2_atk_frame)
+
+        sword_tip_offset = [cos(radians(angle)) * ATTACK_RANGE, sin(radians(angle)) * ATTACK_RANGE] # Think in regular coordinates, we fix later for screen coords
+
+        pygame.draw.line(window, SWORD_COLOR, p2_center, [p2_center[0] + sword_tip_offset[0], p2_center[1] - sword_tip_offset[1]], SWORD_THICKNESS)
+
+    # Display text of all game state information (usefull for debugging)
+    default_font = pygame.font.Font("freesansbold.ttf", 20) # Load default font shipped with pygame
+
+    # Player 1 stats in formatted string
+    p1_stats = f"Player 1 HP: {game_state.p1_hp}\nPlayer 1 X: {game_state.p1_x}\nP1 Attack Frame: {game_state.p1_atk_frame}"
+
+    # Player 2 stats in formatted string
+    p2_stats = f"Player 2 HP: {game_state.p2_hp}\nPlayer 2 X: {game_state.p2_x}\nP2 Attack Frame: {game_state.p2_atk_frame}"
+
+    p1_text = default_font.render(p1_stats, True, (0, 0, 0))
+    p2_text = default_font.render(p2_stats, True, (0, 0, 0))
+
+    p1_textRect = p1_text.get_rect()
+    p1_textRect.center = (100, 50)
+
+    p2_textRect = p2_text.get_rect()
+    p2_textRect.center = (WINDOW_WIDTH - 100, 50)
+
+    window.blit(p1_text, p1_textRect)
+    window.blit(p2_text, p2_textRect)
+
+    pygame.display.update()
+
+# Takes a game state and input state and returns a NEW game state which is next frame based on those inputs
+def update_state(current_game_state: GameState, p1_control_state: ControlState, p2_control_state: ControlState) -> GameState:
+    new_game_state = copy.deepcopy(current_game_state) # Copy current game state but with independent values
+
+    # Move Player 1
+    if p1_control_state.mv_l and not p1_control_state.mv_r: # Will not move if both move controls pressed!
+        new_game_state.p1_x -= PLAYER_SPEED
+        # Check left wall collision
+        if new_game_state.p1_x < PLAYER_SIZE // 2:
+            new_game_state.p1_x = PLAYER_SIZE // 2
+
+    elif p1_control_state.mv_r and not p1_control_state.mv_l:
+        new_game_state.p1_x += PLAYER_SPEED
+        # Check right wall collision
+        if new_game_state.p1_x > WINDOW_WIDTH - (PLAYER_SIZE // 2):
+            new_game_state.p1_x = WINDOW_WIDTH - (PLAYER_SIZE // 2)
+
+    # Process Player 1's attack
+    if current_game_state.p1_atk_frame == 0: # p1 has not started an attack
+        if p1_control_state.atk: # Start an attack if the control is pressed
+            new_game_state.p1_atk_frame += 1
+
+    elif current_game_state.p1_atk_frame < ATTACK_FRAMES: # p1's attack is in progress
+        new_game_state.p1_atk_frame += 1
+
+    elif current_game_state.p1_atk_frame == ATTACK_FRAMES: # p1 has just completed an attack
+        # Process potential damage to p2
+        # Player is damaged if standing within attack range + half of player size, but ahead of p1's center
+        player_distance = new_game_state.p2_x - new_game_state.p1_x
+        if player_distance <= ATTACK_RANGE + (PLAYER_SIZE // 2) and player_distance >= 0:
+            new_game_state.p2_hp -= ATTACK_DAMAGE
+        new_game_state.p1_atk_frame = 0 # reset p1's attack
+
+    # Move Player 2
+    if p2_control_state.mv_l and not p2_control_state.mv_r: # Will not move if both move controls pressed!
+        new_game_state.p2_x -= PLAYER_SPEED
+        # Check left wall collision
+        if new_game_state.p2_x < PLAYER_SIZE // 2:
+            new_game_state.p2_x = PLAYER_SIZE // 2
+
+    elif p2_control_state.mv_r and not p2_control_state.mv_l:
+        new_game_state.p2_x += PLAYER_SPEED
+        # Check right wall collision
+        if new_game_state.p2_x > WINDOW_WIDTH - (PLAYER_SIZE // 2):
+            new_game_state.p2_x = WINDOW_WIDTH - (PLAYER_SIZE // 2)
+
+    # Process Player 2's attack
+    if current_game_state.p2_atk_frame == 0: # p2 has not started an attack
+        if p2_control_state.atk: # Start an attack if the control is pressed
+            new_game_state.p2_atk_frame += 1
+
+    elif current_game_state.p2_atk_frame < ATTACK_FRAMES: # p2's attack is in progress
+        new_game_state.p2_atk_frame += 1
+
+    elif current_game_state.p2_atk_frame == ATTACK_FRAMES: # p2 has just completed an attack
+        # Process potential damage to p1
+        # Player is damaged if standing within attack range + half of player size, but ahead of p2's center
+        player_distance = new_game_state.p2_x - new_game_state.p1_x
+        if player_distance <= ATTACK_RANGE + (PLAYER_SIZE // 2) and player_distance >= 0:
+            new_game_state.p1_hp -= ATTACK_DAMAGE
+        
+        new_game_state.p2_atk_frame = 0 # reset p2's attack
+
+    return new_game_state
